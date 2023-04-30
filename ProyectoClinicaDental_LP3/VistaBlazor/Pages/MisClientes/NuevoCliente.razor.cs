2ef19d544ffeda6f6d8e50d48f334879912b9c80
kjoel16 <123512229+kjoel16@users.noreply.github.com>
2023-04-29 20:11:52 -0600

    Nuevo-CLiente-incompleto-Joel

---
+++ ProyectoClinicaDental_LP3/VistaBlazor/Pages/MisClientes/NuevoCliente.razor.cs
@@ -0,0 +1,52 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+using VistaBlazor.Interfaces;
+
+namespace VistaBlazor.Pages.MisClientes
+{
+    public partial class NuevoCliente
+    {
+        [Inject] private IClienteServicio clienteServicio { get; set; }
+        [Inject] private NavigationManager navigationManager { get; set; }
+        [Inject] private SweetAlertService Swal { get; set; }
+
+        ClienteM clie = new ClienteM();
+
+        protected async Task Guardar()
+        {
+            if (string.IsNullOrWhiteSpace(clie.IdentidadCliente) || string.IsNullOrWhiteSpace(clie.Nombre))
+            {
+                return;
+            }
+
+            ClienteM ClieExiste = new ClienteM();
+
+            ClieExiste = await clienteServicio.GetPorCodigoAsync(clie.IdentidadCliente);
+
+            if (ClieExiste != null)
+            {
+                if (!string.IsNullOrEmpty(clie.IdentidadCliente))
+                {
+                    await Swal.FireAsync("Advertencia", "Ya existe un Cliente con el mismo código", SweetAlertIcon.Warning);
+                    return;
+                }
+            }
+
+            bool inserto = await clienteServicio.NuevoAsync(clie);
+            if (inserto)
+            {
+                await Swal.FireAsync("Atención", "Cliente Registrado con Exito", SweetAlertIcon.Success);
+            }
+            else
+            {
+                await Swal.FireAsync("Error", "No se pudo Registrar el Cliente", SweetAlertIcon.Error);
+            }
+        }
+
+        protected async Task Cancelar()
+        {
+            navigationManager.NavigateTo("/Clientes");
+        }
+    }
+}
